551a5b09672d0cc37bc057d66c645bcba437b654
Your Name <you@example.com>
2020-01-27 17:03:26 -0500

    stuff

--- js/index.js
+++ js/index.js
@@ -5,6 +5,21 @@ $(document).ready(function () {
 //
 // $(".newWrapper3").addClass('newClass');
 
+if ($('.whiteOuter.second').hasClass('fadeOutUp')) {
+
+  console.log("keurgioeruhiuergboigerbiobregiub")
+$('.two').on('click', function() {
+
+    window.location.href="templates.html";
+})
+
+$('.navy.block2').on('mouseenter', function() {
+$('.navy.block2').css('cursor', 'grab')
+
+})
+
+}
+
 
     var color = '#bdeae6';
     var maxParticles = 80;
@@ -238,6 +253,124 @@ function drawAgain() {
 }
 
 drawAgain();
+
+
+
+var color = '#bdeae6';
+var maxParticles = 80;
+
+
+function drawThird() {
+particlesJS('particles-third', {
+  'particles': {
+    'number': {
+      'value': maxParticles,
+      'density': {
+        'enable': true,
+        'value_area': (maxParticles * 10) * 2
+      }
+    },
+    'color': {
+      'value': color
+    },
+    'shape': {
+      'type': 'circle',
+      'stroke': {
+        'width': 0,
+        'color': '#000000'
+      },
+      'polygon': {
+        'nb_sides': 5
+      },
+    },
+    'opacity': {
+      'value': 1,
+      'random': false,
+      'anim': {
+        'enable': false,
+        'speed': 1,
+        'opacity_min': 1,
+        'sync': false
+      }
+    },
+    'size': {
+      'value': 4,
+      'random': true,
+      'anim': {
+        'enable': false,
+        'speed': 40,
+        'size_min': 0.1,
+        'sync': false
+      }
+    },
+    'line_linked': {
+      'enable': true,
+      'distance': 300,
+      'color': color,
+      'opacity': 1,
+      'width': 1
+    },
+    'move': {
+      'enable': true,
+      'speed': 2,
+      'direction': 'none',
+      'random': false,
+      'straight': false,
+      'out_mode': 'out',
+      'bounce': false,
+      'attract': {
+        'enable': false,
+        'rotateX': 600,
+        'rotateY': 1200
+      }
+    }
+  },
+  'interactivity': {
+    'detect_on': 'canvas',
+    'events': {
+      'onhover': {
+        'enable': true,
+        'mode': 'grab'
+      },
+      'onclick': {
+        'enable': true,
+        'mode': 'push'
+      },
+      'resize': true
+    },
+    'modes': {
+      'grab': {
+        'distance': 140,
+        'line_linked': {
+          'opacity': 1
+        }
+      },
+      'bubble': {
+        'distance': 400,
+        'size': 40,
+        'duration': 2,
+        'opacity': 8,
+        'speed': 3
+      },
+      'repulse': {
+        'distance': 200,
+        'duration': 0.4
+      },
+      'push': {
+        'particles_nb': 4
+      },
+      'remove': {
+        'particles_nb': 2
+      }
+    }
+  },
+  'retina_detect': true
+
+})
+
+}
+
+drawThird();
 
 $('a[href^="#slideDown"]').on('click', function(event) {
 
